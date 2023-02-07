970befa5615c37d6971c946671b8379c9377be6f
akshayalix <akshayalix12@gmail.com>
2023-02-07 19:43:33 +0530

    Rebasing, Bring back text_var

--- main.py
+++ main.py
@@ -21,34 +21,14 @@ app.title('Yotube Video Downloader')
 
 ## Elements
 
-"""text_var = tkinter.StringVar(value="Insert a YouTube URL :")
+text_var = tkinter.StringVar(value="Insert a YouTube URL :")
 
 title = customtkinter.CTkLabel(app, textvariable=text_var, font=('Roboto', 15), text_color='cyan')
-title.pack(padx=10, pady=10)"""
+title.pack(padx=10, pady=10)
 
-## Menu Bar
+## Link input
 
-# Frame
-
-menuframe = customtkinter.CTkFrame(app,
-            width=720, height=20,
-            corner_radius=1, fg_color='#4a4a4a'
-            )
-menuframe.pack(expand=True , fill=tkinter.X, anchor='n')
-
-## Menu Buttons
-
-btnfolder = customtkinter.CTkButton(app,
-            width=40, height=20, border_width=0,
-            corner_radius=8, text="Open Folder"
-            )
-btnfolder.place(rely=0.5, anchor='nw')
-
-btnabout = customtkinter.CTkButton(app,
-            width=40, height=20, border_width=0,
-            corner_radius=8, text="About"
-            )
-btnabout.place(rely=0.5, anchor='e')
+link = customtkinter.CTkEntry(app, font=('Roboto', 15), width=350, height=40)
 
 
 
